1c1e566fa82ebab996594252a0955ac95e494c8c
Alexis Labaste <alexislabaste84100@gmail.com>
2024-11-27 12:22:34 +0100

    squelette projet 1

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter TP2 Projet 1'),
     );
   }
 }
@@ -55,19 +55,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -81,45 +68,82 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.blue,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        children: <Widget>[
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              color: Colors.blue.shade100
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            child: Column(
+            children : <Widget>[
+              const Text(
+                'Gateau aux fruit type fraise de couleur rouge',
+                style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Lexend',
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                )),
+              const Text(
+                'Alors du coup c\'est un gateau... \nAvec des fraises... Et genre c\'est un fruit... \nEt en faite elles sont rouges. \nVasi le gateau ma donnée pendant que je dev la page', 
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Lexend',
+                fontSize: 15,
+                )),
+              Row(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.star, color: Colors.blue.shade400),
+                      Icon(Icons.star, color: Colors.blue.shade400),
+                      Icon(Icons.star, color: Colors.blue.shade400),
+                      const Icon(Icons.star),
+                      const Icon(Icons.star),
+                    ],
+                  ),
+                  const Text('170 Note')
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.dining_rounded, color: Colors.blue.shade400),
+                      const Text('Préparation'),
+                      const Text('25min')
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.timer, color: Colors.blue.shade400),
+                      const Text('Cuisson'),
+                      const Text('1hr')
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.accessibility, color: Colors.blue.shade400),
+                      const Text('Nb. Pers'),
+                      const Text('4-6')
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+          ),
+          Image.asset('images/cake.jpeg',fit: BoxFit.cover, width: 700, height: 1080,)
+        ],
+      ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
